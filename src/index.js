16612c5f61075f8001dd8a3bf0c9952d2df23f19
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-20 16:29:01 -0700

    add basic packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // import packages
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose  from 'mongoose';
 import cors from 'cors';
 
+
+const app = express();
+
+// set up
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());

848088221189e2c36733a82a6531fc9338e04459
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-20 16:55:20 -0700

    add first routes for backend

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import express from 'express';
 import mongoose  from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+// routes
+app.use('/posts', postRoutes); // post routes will start with /posts
 
 // set up
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+// db connector
+const DB_URL = 'mongodb+srv://tianhuaf:tianhuaf0526@cluster0.kwioq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));

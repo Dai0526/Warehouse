dde9dac56b585e05983f89188d635430bfc34da0
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-20 17:00:01 -0700

    add controller example:

---
+++ server/controllers/posts.js
@@ -0,0 +1,4 @@
+// all hanlders for posts routes - code logic here
+export const getPosts = (req, res) => {
+    res.send('api routes works at "/"');
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import { getPosts } from '../controllers/posts.js'
 const router = express.Router();
 
 // localhost:5000/posts
-router.get('/', (req, res) => {
-    res.send('api routes works at "/"');
-});
+router.get('/', getPosts);
 
 
 export default router;

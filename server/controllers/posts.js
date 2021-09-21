6a45c9c3c37434e9b576ec3b8220173ff570e20c
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-20 17:22:49 -0700

    add create post api

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,22 @@
+import post_message from "../models/postMessage.js";
+
 // all hanlders for posts routes - code logic here
-export const getPosts = (req, res) => {
-    res.send('api routes works at "/"');
+export const get_posts = async (req, res) => {
+    try{
+        const posts = await post_message.find();
+        res.status(200).json(posts);
+    }catch(error){
+        res.status(404).json({message: error.message})
+    }
+};
+
+export const create_post = async (req, res) => {
+    const post = req.body;
+    const new_post = new post_message(post);
+    try{
+        await new_post.save();
+        res.status(201).json(new_post);
+    }catch(error){
+        res.status(409).json({message: error.message});
+    }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getPosts } from '../controllers/posts.js'
+import { get_posts, create_post } from '../controllers/posts.js'
 const router = express.Router();
 
 // localhost:5000/posts
-router.get('/', getPosts);
-
+router.get('/', get_posts);
+router.post('/', create_post);
 
 export default router;
